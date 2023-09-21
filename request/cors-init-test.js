0d7db104d69e6e91d4148cd4cb844e895aaf4b2f
Hartaithan <hartaithan@gmail.com>
2023-09-21 22:04:17 +0900

    add request script

---
+++ request/cors-init-test.js
@@ -0,0 +1,39 @@
+const xhr = new XMLHttpRequest();
+
+const url = "https://6502dc12a0f2c1f3faeafdf7.mockapi.io/test/estimates/1";
+
+xhr.open("GET", url, true);
+
+xhr.responseType = "json";
+
+xhr.onload = () => {
+  if (xhr.status === 200) {
+    const data = xhr.response;
+
+    const district = document.querySelector(
+      "#estimates-district .estimates-collapsible-content p"
+    );
+    const complex = document.querySelector(
+      "#estimates-realty-complex .estimates-collapsible-content p"
+    );
+    const flate = document.querySelector(
+      "#eestimates-flate .estimates-collapsible-content p"
+    );
+
+    if (district && data.district) {
+      district.textContent = data.district;
+    }
+    if (complex && data["realty complex"]) {
+      complex.textContent = data["realty complex"];
+    }
+    if (flate && data.flate) {
+      flate.textContent = data.flate;
+    }
+  }
+};
+
+xhr.onerror = () => {
+  console.error("api request error");
+};
+
+xhr.send();
